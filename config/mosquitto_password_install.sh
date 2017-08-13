c44c7930aeb8f0de8213820c966e9e4eaa0b1323
Holger Imbery <holger@imbery.de>
2017-08-13 16:45:54 +0200

    update: removed whiptail

--- config/mosquitto_password_install.sh
+++ config/mosquitto_password_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-username=$(whiptail --passwordbox "please enter your username" 8 78 --title "MQTT username dialog" 3>&1 1>&2 2>&3)
-password=$(whiptail --passwordbox "please enter your password" 8 78 --title "MQTT password dialog" 3>&1 1>&2 2>&3)
+echo  please enter your username:
+read username
+echo  please enter your password:
+read password
 
 sed 's/##awesome_username##/'$username'/g' </mqtt/config/mosquitto_password.sh.template > /mqtt/config/mosquitto_password.sh.temp
 sed 's/##awesome_password##/'$password'/g' </mqtt/config/mosquitto_password.sh.temp > /mqtt/config/mosquitto_password.sh
